0d3eed3ba3def4effa6bcc3d2ffdd5be12b19725
Matthias <m@tth.es>
2019-07-10 13:10:37 +0200

    first version

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+
+apt-get update
+apt-get upgrade -y
+
+apt-get install -y prometheus prometheus-pushgateway prometheus-alertmanager
+
+
+mkdir -p /var/run/prometheus
+chmod 777 /var/run/prometheus
+
+
+apt-get install -y software-properties-common
+add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
+
+curl https://packages.grafana.com/gpg.key | apt-key add -
+
+
+sudo apt-get update
+sudo apt-get install -y grafana
